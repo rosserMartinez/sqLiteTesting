c163bf86f1d1ed9398417a2b3e8fe5bd3195edfa
Rosser Martinez <rosser.martinez@gmail.com>
2019-05-29 06:57:33 -0400

    added base ui for minspec

---
+++ sqLiteTesting/Assets/Scripts/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public string lastName;
+    public string firstName;
+    public string homeTown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ sqLiteTesting/Assets/Scripts/PersonList.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersonList : MonoBehaviour
+{
+    public InputField firstName;
+    public InputField lastName;
+    public InputField homeTown;
+
+    public List<Person> people;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //List<Person> people = new List<Person>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void DisplayPeopleList()
+    {
+
+    }
+
+    //functions for DB interaction
+    void CreatePerson()
+    {
+
+    }
+
+    void UpdatePeopleList()
+    {
+
+    }
+
+}
